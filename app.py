997f3ccbbd4b2a71a0a274377aba4de9bcd275b0
Gourab Kar <joykar07@gmail.com>
2021-04-23 10:31:26 +0530

    Recommit

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK-MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Task(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        t1 = Task(title=title, desc = desc)
+        db.session.add(t1)
+        db.session.commit()
+        
+    all_task = Task.query.all()
+    return render_template('index.html', all_task=all_task)
 
 if __name__ == "__main__":
     app.run(debug=True)
